be66bb8714e15a4234904394347f28b6699b39d4
Vinycz <vin_mendes@hotmail.com>
2022-04-20 12:07:28 -0300

    Banner com textos atualizados e Carrossel.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 var janelaLogin = new bootstrap.Modal(document.getElementById("loginNegado"));
-
+var janelaPromocao = new bootstrap.Modal(document.getElementById("promo1"));
 function rotaCadastro() {
   window.location.href = "../registro.html";
 }
-
 function rotaRecuperar() {
   window.location.href = "../recuperar.html";
 }
-
 function rotaLogin() {
   let email = document.getElementById("email").value;
   let senha = document.getElementById("senha").value;
@@ -16,4 +14,16 @@ function rotaLogin() {
   } else {
     janelaLogin.show();
   }
+}
+
+function alertaPromo() {
+  let titulo = "Promomção 1 [Quebra da Banca]";
+
+  let corpo =
+    "<h6>Super legal</h6> <p><img src='./img/4.png' width='160' height='160'></p> ";
+
+  janelaPromocao.show();
+
+  document.getElementById("titulo").innerHTML = titulo;
+  document.getElementById("corpo").innerHTML = corpo;
 }
